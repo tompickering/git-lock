9c29d11862f092a9bb1bde08eb24fbe0b90cbc58
Tom Pickering <tom@alephspace.net>
2025-04-23 17:15:35 +0100

    Add pre-commit test

--- test.sh
+++ test.sh
@@ -41,6 +41,11 @@ git config user.name "Local 1"
 git config user.email "local1@git.lock"
 if git lock $OPT acquire d0/d1/b.txt; then die; fi
 if ! git lock $OPT acquire d0/b.txt; then die; fi
+echo new >> d0/d1/b.txt
+git add d0/d1/b.txt
+if git commit -m "This should fail"; then die; fi
+git config lock.noblockcommit 1
+if ! git commit -m "This should succeed"; then die; fi
 popd >> /dev/null
 
 pushd LOCAL0 >> /dev/null
